016ff65cf13304267a17a0fc700c85d801c967ce
gabriel3001989 <gabrielrz15.30@gmail.com>
2015-11-19 01:49:11 +0000

    Commit modificaciones en clase

---
+++ Development/restapp-sprint1/app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  has_one :profile
+  enum role: [:user, :operator, :chef, :admin]
+  after_initialize :set_default_role
+
+  after_create :set_default_profile
+
+  def set_default_role
+    self.role ||= :user
+  end
+  
+  #def friendly_name
+  #  if self.default_profile.first_name.nil?
+  #    self.email
+  #  else
+  #    self.profile.full_name
+  #  end
+  #end
+  
+  def friendly_name   
+    self.profile.first_name.nil? ? self.email : self.profile.full_name 
+  end
+
+  #def default_profile
+  #  if self.profile.nil?
+  #    profile = Profile.new
+  #    profile.user = self
+  #    profile.save
+  #    self.save      
+  #  end
+  #  self.profile
+  #end
+  
+  def set_default_profile   
+    profile = Profile.new   
+    profile.user = self   
+    profile.save 
+  end
+end
